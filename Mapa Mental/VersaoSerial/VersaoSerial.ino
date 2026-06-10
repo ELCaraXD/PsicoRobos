fefbf9ccfd3447bdd63ba1decae6447fd57b3d9d
ELCaraXD <202413600011@ifba.edu.br>
2026-06-10 20:53:44 -0300

    colocando um for pra testar a modularizacao

--- Mapa Mental/VersaoSerial/VersaoSerial.ino
+++ Mapa Mental/VersaoSerial/VersaoSerial.ino
@@ -15,22 +15,35 @@ uint8_t pos_Antiga[2];
 //       2
 uint8_t Direcao = 0;
 
-void frente(uint8_t passos,uint8_t vel = 255){
+void frente(uint8_t passos, uint8_t Direcao, uint8_t vel = 255){
   switch (Direcao){
-    case 0: Serial.printf("Indo %d passos para cima", passos);  break;
-    case 1: Serial.printf("Indo %d passos para direita", passos); break;
-    case 2: Serial.printf("Indo %d passos para baixo", passos); break;
-    case 3: Serial.printf("Indo %d passos para esquerda", passos); break;
+    case 0: Serial.printf("Indo %d passos para cima \n", passos);  break;
+    case 1: Serial.printf("Indo %d passos para direita \n", passos); break;
+    case 2: Serial.printf("Indo %d passos para baixo \n", passos); break;
+    case 3: Serial.printf("Indo %d passos para esquerda \n", passos); break;
   }
 }
 
 
 void setup() {
-  
-
+  Serial.begin(115200);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  for (uint8_t i = 0; i <= 10; i++){
+    
+    Serial.print("Movimentos do robo:\nDirecao: ");
+    Serial.println(Direcao);
+    frente(i,Direcao);
+    
+    if (Direcao < 3)
+    { Direcao++; }
+    else
+    { Direcao = 0; }
+
+    delay(2000);
+
+  }
+
 
 }
